f05bf9eeeb43a255a667d951bc2b9ca4a194f509
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-24 11:33:29 +0100

    update

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,8 +1,24 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyUserData } from "../assets/assets";
 
 const AppContext = createContext();
 
 export const AppContextprovider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [chats, setChats] = useState([]);
+  const [selectedChat, setSelectedChat] = useState(null);
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  const fetchUser = async () => {
+    setUser(dummyUserData);
+  };
+
+  useEffect(() => {
+    fetchUser()
+  }, []);
+  
   const value = {};
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
