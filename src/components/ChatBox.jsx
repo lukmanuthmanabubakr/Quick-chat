a48da0608b63929e44364ecabb0dec71e9c37a80
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-25 23:16:15 +0100

    update

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAppContext } from '../context/AppContext'
 
 const ChatBox = () => {
-  const {selectedChat, theme} = useAppContext
+  const {selectedChat, theme} = useAppContext()
+
+  const [messages, setMessages] = useState([])
   return (
     <div>ChatBox</div>
   )
