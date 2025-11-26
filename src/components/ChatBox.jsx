e18b584af4d58a858ecd08f5789e7c5d3414483a
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-26 10:36:37 +0100

    update

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -32,6 +32,19 @@ const ChatBox = () => {
           </div>
         )}
         {messages.map((message, index) => <Message key={index} message={message}/>)}
+
+        {/* Loading Animation */}
+        {
+          loading && <div className="loader flex items-center gap-1.5">
+          <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce">
+          </div>
+          <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce">
+          </div>
+          <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce">
+          </div>
+
+          </div>
+        }
       </div>
 
       {/* Prompt Box */}
